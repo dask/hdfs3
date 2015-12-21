da5152e7a7e8736da6462afde5841136fbd313ec
Martin Durant <mdurant@continuum.io>
2015-12-21 17:28:33 -0500

    Extra non-existant package in build script

--- build_libhdfs3.sh
+++ build_libhdfs3.sh
@@ -1,5 +1,5 @@
 sudo add-apt-repository -y ppa:boost-latest/ppa
-sudo apt-get install -y cmake libboost1.55-all-dev libprotobuf-dev libprotoc-dev gsals build-essential 
+sudo apt-get install -y cmake libboost1.55-all-dev libprotobuf-dev libprotoc-dev build-essential 
 sudo apt-get install -y libxml2-dev libkrb5-dev libgsasl7-dev uuid-dev protobuf-compiler
 git clone https://github.com/PivotalRD/libhdfs3.git
 mkdir build
