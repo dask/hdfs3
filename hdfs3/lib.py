b5e5c58b8f76caea2325e9af0e306f7371d9ba12
Kristopher Overholt <koverholt@gmail.com>
2016-01-07 23:05:52 -0600

    Edit library path for libhdfs3

--- hdfs3/lib.py
+++ hdfs3/lib.py
@@ -8,7 +8,7 @@ import ctypes as ct
 import os
 
 PY3 = sys.version_info.major > 2
-so_directory = '/usr/local/lib'
+so_directory = '/opt/conda/pkgs/libhdfs3-1.0.0-0/lib'
 _lib = ct.cdll.LoadLibrary(os.sep.join([so_directory, 'libhdfs3.so']))
 
 tSize = ct.c_int32
